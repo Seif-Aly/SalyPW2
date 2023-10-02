80620bbd3d0cdac235bab0815bdefe7ee589a302
seif <saly@edu.hse.ru>
2023-10-02 19:06:27 +0300

    Initial Commit

---
+++ SalyPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalyPW2
+//
+//  Created by seif on 2023-10-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
