63cb844e1ff012ef0a3c283078941c9b960cbb82
seif <saly@edu.hse.ru>
2023-10-31 01:34:39 +0300

    HW-3_Point-4

--- SalyPW2/WishStoringViewController.swift
+++ SalyPW2/WishStoringViewController.swift
@@ -11,9 +11,9 @@ final class WishStoringViewController: UIViewController{
     private enum Constants{
         static let tableCornerRadius: CGFloat = 20
         static let tableOffset: CGFloat = 20
-        static let cellNumber = 25
     }
     private let table: UITableView = UITableView(frame: .zero)
+    private var wishArray: [String] = ["I wish to add cells to the table"]
     override func viewDidLoad() {
         view.backgroundColor = .blue
         configureTable()
@@ -22,23 +22,31 @@ final class WishStoringViewController: UIViewController{
 // MARK: - UITableViewDataSource
 extension WishStoringViewController: UITableViewDataSource{
     private func configureTable() {
-     view.addSubview(table)
-     table.backgroundColor = .red
-     table.dataSource = self
-     table.separatorStyle = .none
-     table.layer.cornerRadius = Constants.tableCornerRadius
-
-     table.pin(to: view, Constants.tableOffset)
+        view.addSubview(table)
+        table.backgroundColor = .red
+        table.dataSource = self
+        table.separatorStyle = .none
+        table.layer.cornerRadius = Constants.tableCornerRadius
+        
+        table.pin(to: view, Constants.tableOffset)
+        table.register(WrittenWishCell.self, forCellReuseIdentifier: WrittenWishCell.reuseId)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Constants.cellNumber
+        return wishArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
-            cell.textLabel?.text = "Row \(indexPath.row)"
-            return cell
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: WrittenWishCell.reuseId,
+            for: indexPath
+        )
+        
+        guard let wishCell = cell as? WrittenWishCell else { return cell }
+        
+        wishCell.configure(with: wishArray[indexPath.row])
+        
+        return wishCell
     }
     
     
